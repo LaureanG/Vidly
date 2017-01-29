d986da404bcbbf854e2c2f60ac0ae78c50cf6c53
LaureanG <laurean.gheorghiu@gmail.com>
2017-01-29 09:04:45 +0200

    Old way for custom routes

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -13,11 +13,7 @@ namespace Vidly.Controllers
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek!" };
-            return View(movie);
-            //return Content("Hello World!");
-            //return HttpNotFound();
-            //return new EmptyResult();
-            //return RedirectToAction("Index", "Home", new { page=1, sortBy = "name" });
+            return View(movie);            
         }
 
         public ActionResult Edit(int id)
@@ -36,6 +32,11 @@ namespace Vidly.Controllers
                 sortBy = "Name";
             }
             return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
+
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
         }
     }
 }
